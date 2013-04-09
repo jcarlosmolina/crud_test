487ebc1ba81876e188e34c3869ec0d48d07c84ea
Juan Carlos Molina <jcarlosmolina@gmail.com>
2013-04-09 20:31:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrudTest::Application.config.secret_token = '3b6498ff1d507d961082f67eec5e789038858a62f42f470d08cfcda74f326b2615e219b03f0ff9b79136835e71779c56035d92b742a8c8aad4c92c754961fabc'
